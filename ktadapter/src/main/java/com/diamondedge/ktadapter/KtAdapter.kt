dae9bbff38ef459a1eb49b29255c68ff5ebf2f58
Reed Ellsworth <reed.ellsworth@sling.com>
2019-07-15 22:02:51 -0600

    add callbacks for create/bind/unbind

--- ktadapter/src/main/java/com/diamondedge/ktadapter/KtAdapter.kt
+++ ktadapter/src/main/java/com/diamondedge/ktadapter/KtAdapter.kt
@@ -1,11 +1,8 @@
 package com.diamondedge.ktadapter
 
 import android.util.Log
-import android.util.TypedValue
 import android.view.*
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import java.util.ArrayList
 
 typealias ClickListener<T> = (v: View, vh: KtAdapter.ViewHolder<T>, adapter: KtAdapter<T>) -> Unit
 
@@ -17,32 +14,48 @@ typealias KeyListener<T> = (keyCode: Int, event: KeyEvent, v: View, vh: KtAdapte
 
 typealias SelectListener<T> = (position: Int, item: T?) -> Unit
 
+typealias CreateViewHolderListener<T> = (vh: KtAdapter.ViewHolder<T>) -> Unit
+typealias BindViewHolderListener<T> = (vh: KtAdapter.ViewHolder<T>, position: Int, item: T) -> Unit
+typealias UnbindViewHolderListener<T> = (vh: KtAdapter.ViewHolder<T>) -> Unit
+
 abstract class KtAdapter<T : Any> : RecyclerView.Adapter<KtAdapter.ViewHolder<T>>() {
     var clickListener: ClickListener<T>? = null
     var longClickListener: LongClickListener<T>? = null
     var focusListener: FocusListener<T>? = null
     var keyListener: KeyListener<T>? = null
+    var createViewHolderListener: CreateViewHolderListener<T>? = null
+    var bindViewHolderListener: BindViewHolderListener<T>? = null
+    var unbindViewHolderListener: UnbindViewHolderListener<T>? = null
 
     abstract operator fun get(index: Int): T
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): KtAdapter.ViewHolder<T> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder<T> {
         val vh = createVH(parent, viewType, this)
         val view = vh.itemView
         view.setOnClickListener(vh)
         view.setOnLongClickListener(vh)
         view.setOnFocusChangeListener(vh)
         view.setOnKeyListener(vh)
-        // click/focus/key listeners should never be set after onCreateViewHolder!!!!
-        // this is why we use a delegate
+        createViewHolderListener?.invoke(vh)
         return vh
     }
 
     abstract fun createVH(parent: ViewGroup, viewType: Int, adapter: KtAdapter<T>): ViewHolder<T>
 
-    override fun onBindViewHolder(holder: KtAdapter.ViewHolder<T>, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder<T>, position: Int) {
         val item = get(position)
         Log.i("KtAdapter", "onBindViewHolder item($position) $item count: $itemCount")
         holder.bind(position, item)
+        bindViewHolderListener?.invoke(holder, position, item)
+    }
+
+    open fun onUnbindViewHolder(holder: ViewHolder<T>) {
+        unbindViewHolderListener?.invoke(holder)
+    }
+
+    override fun onViewRecycled(holder: ViewHolder<T>) {
+        holder.unbind()
+        onUnbindViewHolder(holder)
     }
 
     open class ViewHolder<T : Any>(itemView: View, val adapter: KtAdapter<T>) : RecyclerView.ViewHolder(itemView),
@@ -54,7 +67,7 @@ abstract class KtAdapter<T : Any> : RecyclerView.Adapter<KtAdapter.ViewHolder<T>
         open fun bind(position: Int, item: T) {}
         open fun unbind() {}
 
-        final override fun onClick(v: View) {
+        override fun onClick(v: View) {
             adapter.clickListener?.invoke(v, this, adapter)
         }
 
